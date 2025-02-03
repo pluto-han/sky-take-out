d3c21866ca8d8d539eba80409cd359826d2327d4
pluto-han <1514142997@qq.com>
2025-02-03 23:54:01 +0800

    新增员工业务代码开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,23 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class EmployeeDTO implements Serializable {
-
+    @ApiModelProperty("主键id")
     private Long id;
-
+    @ApiModelProperty("用户名")
     private String username;
-
+    @ApiModelProperty("姓名")
     private String name;
-
+    @ApiModelProperty("手机号")
     private String phone;
-
+    @ApiModelProperty("性别")
     private String sex;
-
+    @ApiModelProperty("身份证号")
     private String idNumber;
 
 }
--- sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.sky.entity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,31 +16,33 @@ import java.time.LocalDateTime;
 public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @ApiModelProperty("主键id")
     private Long id;
-
+    @ApiModelProperty("用户名")
     private String username;
-
+    @ApiModelProperty("姓名")
     private String name;
-
+    @ApiModelProperty("密码")
     private String password;
-
+    @ApiModelProperty("手机号")
     private String phone;
-
+    @ApiModelProperty("性别")
     private String sex;
-
+    @ApiModelProperty("身份证号")
     private String idNumber;
-
+    @ApiModelProperty("员工状态")
     private Integer status;
 
     //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty("创建时间")
     private LocalDateTime createTime;
 
     //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty("修改时间")
     private LocalDateTime updateTime;
-
+    @ApiModelProperty("创建人")
     private Long createUser;
-
+    @ApiModelProperty("修改人")
     private Long updateUser;
 
 }
